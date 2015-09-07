b4b0ff4fe44ef6059d115a70e35ce2a2bd8214e7
Joel Herb <he_qiao_2010@yeah.net>
2015-09-07 15:39:58 +0800

    Update Test.java

--- src/com/github/heqiao2010/Test.java
+++ src/com/github/heqiao2010/Test.java
@@ -1,5 +1,10 @@
 package com.github.heqiao2010;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Calendar;
 
 public class Test {
@@ -8,7 +13,9 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args){
+		test1();
 		test2();
+		test3();
 	}
 	
 	private static void test1(){
@@ -52,35 +59,44 @@ public class Test {
 		c1.set(Calendar.YEAR, 1900);
 		c1.set(Calendar.MONTH, 0);
 		c1.set(Calendar.DATE, 30);
-		for(int i=0; i<73019; i++){
-			c1.add(Calendar.DATE, 1);
-			LunarCalendar luanr = LunarCalendar.solar2Lunar(c1);
-			System.out.println("Solar：" + df.format(c1.getTime()) + "Lunar：" + luanr);
+		FileOutputStream out = null;
+		PrintStream p = null;
+		try {
+			File testFile = new File("./solar2lunar.txt");
+			if(!testFile.exists()){
+				testFile.createNewFile();
+			}
+			out = new FileOutputStream(testFile);
+			p = new PrintStream(out);
+			for(int i=0; i<73019; i++){
+				c1.add(Calendar.DATE, 1);
+				LunarCalendar luanr = LunarCalendar.solar2Lunar(c1);
+				p.println("Solar：" + df.format(c1.getTime()) + " <====> Lunar：" + luanr.getFullLunarName());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("未找到solar2lunar.txt文件，或者文件创建失败.");
+			e.printStackTrace();
+		} catch (IOException e){
+			System.out.println("未找到solar2lunar.txt文件，或者文件创建失败.");
+			e.printStackTrace();
+		}finally {
+			try {
+				out.close();
+				p.close();
+			} catch (IOException e) {
+				System.out.println("关闭流出错。");
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	private static void test3(){
 		java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("yyyy-MM-dd");
 		Calendar c1 = Calendar.getInstance();
-		/**
-			Solar：2017-12-18Lunar：Wrong lunar date.
-			Solar：2017-12-19Lunar：Wrong lunar date.
-			Solar：2017-12-20Lunar：Wrong lunar date.
-			Solar：2017-12-21Lunar：Wrong lunar date.
-			Solar：2017-12-22Lunar：Wrong lunar date.
-			Solar：2017-12-23Lunar：Wrong lunar date.
-			Solar：2017-12-24Lunar：Wrong lunar date.
-			Solar：2017-12-25Lunar：Wrong lunar date.
-			Solar：2017-12-26Lunar：Wrong lunar date.
-			Solar：2017-12-27Lunar：Wrong lunar date.
-			Solar：2017-12-28Lunar：Wrong lunar date.
-			Solar：2017-12-29Lunar：Wrong lunar date.
-			Solar：2017-12-30Lunar：Wrong lunar date.
-			Solar：2017-12-31Lunar：Wrong lunar date.
-		 */
-		c1.set(Calendar.YEAR, 2017);
-		c1.set(Calendar.MONTH, 11);
-		c1.set(Calendar.DATE, 31);
+		
+		c1.set(Calendar.YEAR, 2046);
+		c1.set(Calendar.MONTH, 01);
+		c1.set(Calendar.DATE, 06);
 		LunarCalendar luanr = LunarCalendar.solar2Lunar(c1);
 		System.out.println();
 		System.out.println("Solar：" + df.format(c1.getTime()) + "Lunar：" + luanr);
