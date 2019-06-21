a87fa218505dde5da7f3f00f47333a772eba6f19
qiao.he <qiao.he@qingteng.cn>
2019-06-21 16:39:51 +0800

    [fix]解决javadoc错误和警告。

--- lunar/src/main/java/com/github/heqiao2010/lunar/LunarCalendar.java
+++ lunar/src/main/java/com/github/heqiao2010/lunar/LunarCalendar.java
@@ -47,8 +47,8 @@ public final class LunarCalendar implements Serializable {
             "卅十"};
     /**
      * 农历信息.<br>
-     * <li>每个数组的第一个数表示该年闰月月份，为0表示不闰月</li>
-     * <li>数组中其他数表示该月初一对应的公历日期</li>
+     * 每个数组的第一个数表示该年闰月月份，为0表示不闰月<br>
+     * 数组中其他数表示该月初一对应的公历日期<br>
      */
     protected final static int[][] LUAR_INFO = {
             {8, 131, 301, 331, 429, 528, 627, 726, 825, 924, 1023, 1122, 1222, 1320}, // 1900
@@ -310,6 +310,9 @@ public final class LunarCalendar implements Serializable {
     // 公历日期
     private GregorianCalendar solar = new GregorianCalendar();
 
+    /**
+     * 默认构造
+     */
     public LunarCalendar() {
         computeBySolarDate(solar.get(Calendar.YEAR), solar.get(Calendar.MONTH), solar.get(Calendar.DATE));
     }
@@ -328,6 +331,8 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 通过公历构造
+     *
+     * @param calendar 　公历日期
      */
     public LunarCalendar(Calendar calendar) {
         computeBySolarDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE));
@@ -335,6 +340,11 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 计算两个农历日期之差
+     *
+     * @param lc1   　农历１
+     * @param lc2   　农历２
+     * @param field 　计算的维度，比如按月,天等
+     * @return 具体的差值
      */
     public static long luanrDiff(LunarCalendar lc1, LunarCalendar lc2, int field) {
         return solarDiff(lc1.getSolar(), lc2.getSolar(), field);
@@ -342,6 +352,9 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 公历转农历
+     *
+     * @param solar 　公历日期
+     * @return 农历日期
      */
     public static LunarCalendar solar2Lunar(Calendar solar) {
         LunarCalendar ret = new LunarCalendar();
@@ -351,6 +364,12 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 农历转公历
+     *
+     * @param lunarYear   　农历年
+     * @param lunarMonth  　农历月，从１开始
+     * @param LunarDate   　农历日
+     * @param isLeapMonth 　是否润月
+     * @return 公历日期
      */
     public static Calendar lunar2Solar(int lunarYear, int lunarMonth, int LunarDate, boolean isLeapMonth) {
         LunarCalendar ret = new LunarCalendar();
@@ -359,7 +378,10 @@ public final class LunarCalendar implements Serializable {
     }
 
     /**
-     * 获取天
+     * 获取农历日的表示
+     *
+     * @param lunarDay 　农历日数值表示
+     * @return 农历日传统字符表示
      */
     public static String getDayName(int lunarDay) {
         return LunarDayName[lunarDay - 1];
@@ -367,13 +389,19 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 获取农历月份
+     *
+     * @param lunarMonth 　农历月数值表示
+     * @return 农历月传统字符表示
      */
     public static char getMonthName(int lunarMonth) {
         return LunarMonthName[lunarMonth - 1];
     }
 
     /**
-     * 获取年
+     * 获取农历年份
+     *
+     * @param lunarYear 　农历年数值表示
+     * @return 农历年传统字符表示
      */
     public static String getYearName(int lunarYear) {
         StringBuilder sb = new StringBuilder();
@@ -387,6 +415,11 @@ public final class LunarCalendar implements Serializable {
     /**
      * 判断两个整数所代表公历日期的差值<br>
      * 一年按365天计算，一个月按30天计算<br>
+     *
+     * @param solarCode1 　农历日期代码
+     * @param solarCode2 　农历日期代码
+     * @param field      　差值单位
+     * @return 差值
      */
     public static long solarDateCodesDiff(int solarCode1, int solarCode2, int field) {
         GregorianCalendar c1 = new GregorianCalendar(solarCode1 / 10000, solarCode1 % 10000 / 100 - 1,
@@ -399,6 +432,11 @@ public final class LunarCalendar implements Serializable {
     /**
      * 求两个公历日期之差，field可以为年月日，时分秒<br>
      * 一年按365天计算，一个月按30天计算<br>
+     *
+     * @param solar1 　历日期
+     * @param solar2 　历日期
+     * @param field  差值单位
+     * @return 差值
      */
     public static long solarDiff(Calendar solar1, Calendar solar2, int field) {
         long t1 = solar1.getTimeInMillis();
@@ -423,8 +461,12 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 一个简单的二分查找，返回查找到的元素坐标，用于查找农历二维数组信息
+     *
+     * @param array 　数组
+     * @param n     　待查询数字
+     * @return 查到的坐标
      */
-    public static int binSearch(int[] array, int n) {
+    private static int binSearch(int[] array, int n) {
         if (null == array || array.length == 0) {
             return -1;
         }
@@ -456,8 +498,8 @@ public final class LunarCalendar implements Serializable {
     /**
      * 返回传统天干地支年名称
      *
-     * @param y 注意这里是农历年
-     * @return String
+     * @param y 农历年
+     * @return 传统农历年份的表示
      */
     public static String getTraditionalYearName(int y) {
         y = y - MINI_YEAR + 36;
@@ -466,6 +508,9 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 获取生肖名
+     *
+     * @param y 　农历年
+     * @return 生肖名
      */
     public static char getAnimalYearName(int y) {
         return LunarAnimailName[(y - 4) % 12];
@@ -474,6 +519,9 @@ public final class LunarCalendar implements Serializable {
     /**
      * 创建LunarInfo中某一年的一列公历日历编码<br>
      * 公历日历编码：年份+月份+天，用于查询某个公历日期在某个LunarInfo列的哪一个区间<br>
+     *
+     * @param solarYear 年份
+     * @return 公历日历编码
      */
     private int[] builderSolarCodes(int solarYear) {
         if (solarYear < MINI_YEAR && solarYear > MAX_YEAR) {
@@ -499,7 +547,11 @@ public final class LunarCalendar implements Serializable {
     }
 
     /**
-     * 日期增加
+     * 日期增加,和<code>GregorianCalendar.add</code>类似
+     *
+     * @param field  　单位
+     * @param amount 数值
+     * @see GregorianCalendar
      */
     public void add(int field, int amount) {
         this.getSolar().add(field, amount);
@@ -509,6 +561,11 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 通过给定的农历日期，计算公历日期
+     *
+     * @param lunarYear   　农历年
+     * @param lunarMonth  　农历月，从１开始
+     * @param lunarDate   　农历日期
+     * @param isleapMonth 　是否为闰月
      */
     private void computeByLunarDate(final int lunarYear, final int lunarMonth, final int lunarDate, final boolean isleapMonth) {
         if (lunarYear < MINI_YEAR && lunarYear > MAX_YEAR) {
@@ -607,12 +664,22 @@ public final class LunarCalendar implements Serializable {
 
     /**
      * 增加公历日期
+     *
+     * @param field 　单位
+     * @param n     数值
+     * @see GregorianCalendar
      */
     public void solarAdd(int field, int n) {
         getSolar().add(field, n);
         computeBySolarDate(getSolar().get(Calendar.YEAR), getSolar().get(Calendar.MONTH), getSolar().get(Calendar.DATE));
     }
 
+    /**
+     * 返回农历日期，不包含年份
+     *
+     * @param showLeap 　是否显示闰月的闰字
+     * @return 农历日期
+     */
     public String getLunar(boolean showLeap) {
         if (this.lmonth < 1 || this.lmonth > 12 || this.ldate < 1
                 || this.ldate > 30) {
@@ -659,8 +726,6 @@ public final class LunarCalendar implements Serializable {
         return solar;
     }
 
-    //---------------农历数据---------------
-
     public void setSolar(GregorianCalendar solar) {
         this.solar = solar;
     }
@@ -681,7 +746,7 @@ public final class LunarCalendar implements Serializable {
     public String toString() {
         if (this.lyear < MINI_YEAR || this.lyear > MAX_YEAR || this.lmonth < 1 || this.lmonth > 12 || this.ldate < 1
                 || this.ldate > 30) {
-            throw new LunarException("Wrong lunar ldate: " + lyear + " " + lmonth + " " + ldate);
+            return "Wrong lunar date: " + lyear + " " + lmonth + " " + ldate;
         }
         return getYearName(this.lyear) + "年" + (this.isLeapMonth() ? "闰" : "") + getMonthName(this.lmonth) + "月"
                 + getDayName(this.ldate);
